24140f2602110adc5ed505d2eec1d289ec5ead3d
Nico Pelsmaekers <nico.pelsmaekers@student.kdg.be>
2017-02-08 11:02:22 +0100

    branches test file with first tests

--- src/testing/app.component.spec.ts
+++ src/testing/app.component.spec.ts
@@ -1,5 +1,3 @@
-
-
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from '../app/app.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
---
+++ src/testing/branches.component.spec.ts
@@ -0,0 +1,30 @@
+import { TestBed, async } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {BranchComponent} from "../branches/branches.component";
+
+
+describe('AppComponent', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      declarations: [
+        BranchComponent
+      ],
+    });
+    TestBed.compileComponents();
+  });
+
+  it(`should have as title 'Nieuw Filiaal'`, async(() => {
+    const fixture = TestBed.createComponent(BranchComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('Nieuw filiaal!');
+
+  }));
+
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(BranchComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Nieuw filiaal');
+  }));
+});
