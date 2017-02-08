2f8e6362ecd5ffaeee8cddfa5fb6d468ff21857c
Emmanuelkouar <emmanuel.kouar@student.kdg.be>
2017-02-08 11:00:46 +0100

    Company first testing

---
+++ src/testing/company.component.spec.ts
@@ -0,0 +1,32 @@
+
+
+import { TestBed, async } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {CompanyComponent} from "../company/company.component";
+
+
+describe('CompanyComponent', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      declarations: [
+        CompanyComponent
+      ],
+    });
+    TestBed.compileComponents();
+  });
+
+  it(`should have as title 'Nieuw bedrijf!'`, async(() => {
+    const fixture = TestBed.createComponent(CompanyComponent);
+    const company = fixture.debugElement.componentInstance;
+    expect(company.title).toEqual('Nieuw bedrijf!');
+
+  }));
+
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(CompanyComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Nieuw bedrijf!');
+  }));
+});
