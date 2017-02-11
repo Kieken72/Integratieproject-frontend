7d18c9006e8255269e51dcd1a8c82365f00362b6
Emmanuelkouar <emmanuel.kouar@student.kdg.be>
2017-02-12 00:11:27 +0100

    Path for companies changed to: /manage/companies Todo: http://localhost:4200/manage/company/add

--- src/app/company/model/company.ts
+++ src/app/company/model/company.ts
@@ -1,8 +1,10 @@
+import {City} from "./city";
 export class Company{
   public Name : string;
   public VATNumber: string;
   public Street: string;
   public Number: string;
   public Box: string;
-  public CityId: string;
+  //public CityId:string;
+  public City: City;
 }
