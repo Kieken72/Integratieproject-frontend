5b0530c56b13b8ab4d68b2c26fb65b665c2aef14
Emmanuelkouar <emmanuel.kouar@student.kdg.be>
2017-02-22 12:08:02 +0100

    test for end2endtest doc

--- src/app/shared/login/login.component.ts
+++ src/app/shared/login/login.component.ts
@@ -12,35 +12,10 @@ declare const FB:any;
 export class LoginComponent implements OnInit {
 
   constructor(private userService: UserService, private router: Router) {
-    FB.init({
-      //TODO:create app on facebook for ID
-      appId: 'our app ID',
-      cookie: false,
-      xfbml: true,
-      version:'v2.5'
-    })
-
   }
   ngOnInit() {
-    FB.getLoginStatus(response => {
-      this.statusChangeCallback(response);
-    });
 
   }
-  onFacebookLoginClick() {
-    FB.login();
-  }
-
-  statusChangeCallback(resp) {
-    if (resp.status === 'connected') {
-      // connect here with your server for facebook login by passing access token given by facebook
-    }else if (resp.status === 'not_authorized') {
-
-    }else {
-
-    }
-  };
-
 
   onSubmit(email, password){
     this.userService.login(email, password).subscribe((result) => {
@@ -49,5 +24,4 @@ export class LoginComponent implements OnInit {
         }
     })
   }
-
 }
