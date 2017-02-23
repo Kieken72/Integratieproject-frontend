bf26da60001165b4231b7ae792c77b46d11a8c02
Nico Pelsmaekers <nico.pelsmaekers@student.kdg.be>
2017-02-23 11:58:14 +0100

    booker search e2e

--- e2e/booker-search.e2e-spec.ts
+++ e2e/booker-search.e2e-spec.ts
@@ -23,19 +23,29 @@ describe('Testbooker2', ()=> {
    expect(companies.count()).toBeGreaterThan(1);
    });*/
 
-  it('should check page 2222', () => {
+  it('should check waar label', () => {
     browser.get('/booker/search');
     const whereLocator = by.id('waar');
-    const whereelement = element(whereLocator)
-
+    const whereelement = element(whereLocator);
     expect(whereelement.getText()).toBe('Waar');
   });
-  it('should check date label 222', () => {
+  it('should check date label ', () => {
     browser.get('/booker/search');
-    const dateLocator = by.id('date');
-    const whereelement = element(dateLocator)
-
+    const dateLocator = by.id('lbldate');
+    const whereelement = element(dateLocator);
     expect(whereelement.getText()).toBe('Datum');
+  });
+  it('should go to booker/list', () => {
+    browser.get('/booker/search');
+    const whereLocator = by.tagName('typeahead');
+    const whereelement = element(whereLocator);
+    whereelement.click();
+    whereelement.sendKeys('Kontich');
+    const searchButton = element.all(by.css('btn btn-default')).first();
+    searchButton.click();
+    const branchNameLocator = element.all(by.css('hvr-underline-from-center')).first();
+    const branchElement = element(branchNameLocator);
+    expect(branchElement.getText()).toBe('Filiaal Kontich');
   });
 })
 
