989bea92b8855f2e39d83b3837802d347b98c7bc
Emmanuelkouar <emmanuel.kouar@student.kdg.be>
2017-02-24 16:09:39 +0100

    Profile page + profile reservation + reservation ID to reservation name

---
+++ src/app/account/account-detail/model/reservation.ts
@@ -0,0 +1,12 @@
+export class Reservation{
+  public Id: string;
+  public AmountOfPersons: string;
+  public DateTime: string;
+  public EndDateTime: string;
+  public SpaceId: string;
+  public BranchId: string;
+  public AccountId: string;
+  public Messages: string;
+  public Review: string;
+  public Cancelled: string;
+}
