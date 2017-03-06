035d13050c2860e99350144bbfb329cfbb4c9cc8
Emmanuelkouar <emmanuel.kouar@student.kdg.be>
2017-03-07 00:01:01 +0100

    List van berichten werkt + chat layout "tekstballon rechts = huidige user", "tekstballon links = antwoord van andere user" werkt ook. Voorbeeld bij reservatie met ID 2. Laatste ding te doen: auto refresh data na het versturen van een bericht.

--- src/app/booker/booker-reservationdetail/booker-reservationdetail.component.ts
+++ src/app/booker/booker-reservationdetail/booker-reservationdetail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 import {Reservation} from "../../account/account-detail/model/reservation";
 import {Message} from "./model/message";
 import {AccountDetailComponent} from "../../account/account-detail/account-detail.component";
@@ -18,16 +18,18 @@ export class BookerReservationdetailComponent implements OnInit {
   private reservation: Reservation;
   private message:Message = new Message();
   private reservations:Reservation[] = new Array();
+  private currentReservationId:number;
   private messages:Message[];
   private user:User;
   private branches: Branch[];
-  constructor( private route: ActivatedRoute, private profileService: ProfileService, private branchService:BranchService, private reservationService:ReservationService) { }
+  constructor( private route: ActivatedRoute, private rout:Router, private profileService: ProfileService, private branchService:BranchService, private reservationService:ReservationService) { }
 
   ngOnInit() {
     this.getBranches();
     this.route.params
       .switchMap((params: Params) => this.getReservation(+params['id']))
       .subscribe(reservation => this.whenDetailsLoads(reservation));
+    this.getUser();
   }
 
   whenDetailsLoads(reservation){
@@ -36,7 +38,7 @@ export class BookerReservationdetailComponent implements OnInit {
 
   getReservation(number:number){
     this.profileService.getProfile().subscribe((data)=>{
-      //this.getUser(data);
+      //this.getUser();
       data.Reservations.forEach((cReservation) => {
         if(cReservation.Id == number.toString()){
           this.branches.forEach((branch) => {
@@ -45,8 +47,9 @@ export class BookerReservationdetailComponent implements OnInit {
 
             };
           })
-          //this.reservationService.getMessages(cReservation.Id).subscribe((data) => this.messages = data);
+          this.reservationService.getMessages(cReservation.Id).subscribe((data) => this.messages = data);
           this.reservations.push(cReservation);
+          this.currentReservationId = cReservation.Id;
          // this.getMessages(cReservation.Id);
         }
       });
@@ -54,8 +57,8 @@ export class BookerReservationdetailComponent implements OnInit {
     return this.reservations;
   }
 
-  getMessages(number: string){
-    this.reservationService.getMessages(number).subscribe(data => this.messages = data);
+  getUser(){
+    this.profileService.getProfileWithToken(localStorage.getItem('auth_token')).subscribe((data)=>this.user = data);
   }
 
   getBranches():void{
@@ -66,7 +69,9 @@ export class BookerReservationdetailComponent implements OnInit {
     this.reservations.forEach((cRes)=>{
       this.reservationService.postMessage(cRes.Id, cRes.BranchId, this.message.text).subscribe((data)=>console.log(data));
 
+      this.reservationService.getMessages(cRes.Id).subscribe((data) => this.messages = data);
     });
+    //this.rout.navigate(['../../manager/reservationdetail/', this.currentReservationId]);
 
   }
 }
