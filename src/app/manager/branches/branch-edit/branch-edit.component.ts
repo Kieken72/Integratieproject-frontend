d1d02cc1b3bed648a67bcb4817c0dd26c73c448d
Seppe Van Winkel <seppe.vanwinkel@student.kdg.be>
2017-03-13 17:05:09 +0100

    Added + and - for openinghours + fix selected

--- src/app/manager/branches/branch-edit/branch-edit.component.ts
+++ src/app/manager/branches/branch-edit/branch-edit.component.ts
@@ -4,6 +4,18 @@ import {Branch} from "../../../shared/model/branch";
 import {Params, ActivatedRoute} from "@angular/router";
 import {CityService} from "../../../shared/cityservice/city.service";
 import {City} from "../../../shared/cityservice/city";
+import {OperationHour} from "../../../shared/model/operationhour";
+
+export class Weekday{
+  public Id: number;
+  public Day: string;
+
+  constructor(Id: number, Day: string) {
+    this.Id = Id;
+    this.Day = Day;
+  }
+}
+
 
 @Component({
   selector: 'app-branch-edit',
@@ -13,12 +25,23 @@ import {City} from "../../../shared/cityservice/city";
 export class BranchEditComponent implements OnInit {
 
   private branch :Branch;
+  private days: Weekday[];
 
   cities: City[];
   private selectedCity:any;
   //cityid: number;
   city: City;
-  constructor(private route: ActivatedRoute,private branchService: BranchService, private cityService:CityService) { }
+  constructor(private route: ActivatedRoute,private branchService: BranchService, private cityService:CityService) {
+    this.days = [
+      new Weekday(0,"Sunday"),
+      new Weekday(1,"Monday"),
+      new Weekday(2,"Thuesday"),
+      new Weekday(3,"Wednesday"),
+      new Weekday(4,"Thursday"),
+      new Weekday(5,"Friday"),
+      new Weekday(6,"Saturday")
+    ];
+  }
 
   ngOnInit() {
     this.getCities();
@@ -44,6 +67,15 @@ export class BranchEditComponent implements OnInit {
     this.branchService.putBranch(
       this.branch.Id, this.branch.Name, this.branch.Street, this.branch.Number, this.branch.Box,
       this.branch.CityId, this.branch.PhoneNumber, this.branch.Email,this.branch.Description,this.branch.AdditionalInfos,this.branch.OpeningHours) ;
+  }
+
+  removeOperationHour(entity){
+    this.branch.OpeningHours = this.branch.OpeningHours.filter(item => item !== entity);
+  }
+ addOperationHour(){
+    console.log(this.branch.OpeningHours.length);
+    this.branch.OpeningHours.push(new OperationHour());
+   console.log(this.branch.OpeningHours.length);
   }
 
 }
