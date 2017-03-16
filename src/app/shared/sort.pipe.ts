62ffea27f72654cea92d1c4fdb643ecc47c96872
Seppe Van Winkel <seppe.vanwinkel@student.kdg.be>
2017-03-16 14:47:57 +0100

    Sortpipe + Testing Pipe

---
+++ src/app/shared/sort.pipe.ts
@@ -0,0 +1,37 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sort'
+})
+export class SortPipe implements PipeTransform {
+
+  transform(array: Array<string>, args: string): Array<string> {
+    if(!array){
+      return array;
+    }
+
+    if(!args){
+      array.sort((a: any, b: any) => {
+        if ( a < b ){
+          return -1;
+        }else if( a > b ){
+          return 1;
+        }else{
+          return 0;
+        }
+      });
+      return array;
+    }
+    array.sort((a: any, b: any) => {
+      if ( a[args] < b[args] ){
+        return -1;
+      }else if( a[args] > b[args] ){
+        return 1;
+      }else{
+        return 0;
+      }
+    });
+    return array;
+  }
+
+}
