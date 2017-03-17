91c389fd6c0b35788d4f299d0137b550845aa13d
Nico Pelsmaekers <nico.pelsmaekers@student.kdg.be>
2017-03-17 16:32:12 +0100

    reservatie maken in kalender

--- src/app/manager/manager-calendar/manager-calendar.component.ts
+++ src/app/manager/manager-calendar/manager-calendar.component.ts
@@ -4,6 +4,12 @@ import {Reservation} from "../../shared/model/reservation";
 import {ManagerService} from "../manager.service";
 import {ModalDirective} from "ng2-bootstrap";
 import {ShortUser} from "../../shared/model/short-user";
+import {SearchService} from "../../booker/shared/search.service";
+import {BookerSearch} from "../../booker/shared/model/booker-search";
+import {Branch,CheckMessage} from "../../shared/model/branch";
+import {Newreservation} from "../../shared/model/newreservation";
+import {ManagerCheckbranch} from "../../shared/model/manager-checkbranch";
+import {ManagerReservation} from "../../shared/model/ManagerReservation";
 
 @Component({
   selector: 'app-manager-calendar',
@@ -17,12 +23,23 @@ export class ManagerCalendarComponent implements OnInit {
 
   private reservations:Reservation[];
   private date:Date = new Date();
+  private endDate:Date= new Date();
   private users: ShortUser[];
+  private userId:string;
+  private search: BookerSearch;
+  private reservation: Reservation;
+  private reservationResponse: Branch;
+  private amount:number=2;
+  public CheckMessage = CheckMessage;
+  private persons: number[];
 
   private currentReservation:Reservation;
-  constructor(private reservationService:ReservationService, private managerService: ManagerService) { }
+  constructor(private reservationService:ReservationService, private managerService: ManagerService, private searchService:SearchService) { }
 
   ngOnInit() {
+    this.search = this.searchService.searchParameters;
+    this.persons = this.searchService.persons;
+
     console.log(this.managerService.branchId);
     this.refreshReservations();
     this.managerService.getGuestsByBranch(this.managerService.branchId).subscribe(data => this.getUsers(data));
@@ -36,6 +53,7 @@ export class ManagerCalendarComponent implements OnInit {
 
   refreshReservations(){
     console.log(this.date);
+    console.log(this.managerService.branchId);
     this.reservationService.getReservationByBranch(this.managerService.branchId, this.date).subscribe(data=>this.setReservations(data));
   }
 
@@ -87,7 +105,7 @@ export class ManagerCalendarComponent implements OnInit {
   }
   public today(){
     let newDate = new Date();
-    this.date = newDate
+    this.date = newDate;
     this.refreshReservations();
   }
 
@@ -96,4 +114,55 @@ export class ManagerCalendarComponent implements OnInit {
     this.refreshReservations();
   }
 
+
+  private dateChanged() {
+    var date = this.date;
+    this.endDate = date;
+    this.endDate.setHours(this.date.getHours()+2);
+    this.refreshPlace();
+  }
+
+  private processDate(){
+    var date = this.date;
+    date.setHours(this.search.time.getHours()+1);
+    date.setMinutes(this.search.time.getMinutes());
+
+    return date;
+  }
+
+  private processEndDate(){
+    var date = this.endDate;
+    date.setHours(this.search.time.getHours()+1);
+    date.setMinutes(this.search.time.getMinutes());
+
+    return date;
+  }
+
+  refreshPlace(){
+      this.reservationResponse = null;
+
+      var param = new ManagerCheckbranch();
+      param.StartDate = this.processDate();
+      param.EndDate = this.processEndDate();
+      param.Amount = this.amount;
+      console.log(this.amount);
+      this.reservationService.isBranchAvailableManager(this.managerService.branchId, param).subscribe(data=>this.processIsPlace(data));
+
+  }
+
+  processIsPlace(data){
+    this.reservationResponse = data;
+    console.log(this.reservationResponse);
+  }
+
+
+  reservePlace(){
+    var newReservation = new ManagerReservation();
+    newReservation.BranchId = this.managerService.branchId;
+    newReservation.Amount = this.amount;
+    newReservation.Id = this.userId;
+    newReservation.StartDate = this.processDate();
+    newReservation.EndDate = this.processEndDate()
+    this.reservationService.postManagerReservation(newReservation).subscribe(data=>this.reservation=data,err=>console.log(err));
+  }
 }
